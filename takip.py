55839ddaa7f69c6fc560fa3e70da55cc9d7dce37
aykutkadiruzun-ops <aykutkadiruzun@gmail.com>
2026-06-05 15:23:09 +0300

    Update takip.py

--- takip.py
+++ takip.py
@@ -45,6 +45,7 @@ def zara_fiyat_ve_adi(url):
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
             )
             page = context.new_page()
+            stealth_sync(page)
             page.goto(url, wait_until="domcontentloaded", timeout=60000)
             time.sleep(12)
 
