e01bff76c2529abfb924dc75bfa0e6cf45cfd1c6
aykutkadiruzun-ops <aykutkadiruzun@gmail.com>
2026-06-05 15:28:13 +0300

    Update takip.py

--- takip.py
+++ takip.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import sync_playwright
-from playwright_stealth import stealth_sync
+from playwright_stealth import Stealth
 import time
 import os
 import json
@@ -45,7 +45,7 @@ def zara_fiyat_ve_adi(url):
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
             )
             page = context.new_page()
-            stealth_sync(page)
+            Stealth().apply_stealth(page)
             page.goto(url, wait_until="domcontentloaded", timeout=60000)
             time.sleep(12)
 
